c6bf3945a0d70922356df0a4de8e8f328a41942f
singhharsh199 <singhharsh199@gmail.com>
2026-07-23 21:24:06 +0100

    cleanup: remove junk Claude web scrapings and temporary build artifacts while updating analysis and documentation

--- timecheck.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <mach/mach_time.h>
-int main() {
-    mach_timebase_info_data_t tb;
-    mach_timebase_info(&tb);
-    printf("numer: %d, denom: %d\n", tb.numer, tb.denom);
-    return 0;
-}
